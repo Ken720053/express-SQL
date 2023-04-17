54426a929f77303e7161280ed6ddac0f1dd980a6
Ken <ken720053@gmail.com>
2023-04-17 12:55:38 +0800

    20230417

--- app.js
+++ app.js
@@ -1,55 +1,53 @@
-const express = require("express");
-const req = require("express/lib/request");
-const res = require("express/lib/response");
-const mysql = require("mysql");
-const { JSON } = require("mysql/lib/protocol/constants/types");
+import express, { response } from 'express';
+import mysql from 'mysql';
 
 const pool = mysql.createPool({
     host:'localhost',
     database:'TodoList',
     user:'root',
     password:'James8416801',
+    prot:'3306'
 });
-
 
 const app = express();
 app.use(express.json());
 const port = 3000;
 
+
 //新增users
 app.post('/',async (req,res) =>{
+    // const todoID = req.query.todo_id;
+    // const todoTitle = req.query.todo_title;
     pool.getConnection((err,conn) => {
-        // if(err){
-        //     res.write("Error Connect");
-        //     res.end();
-        // };
-        conn.query("INSERT INTO VALUES (? , ? )",
-        [req.body.id , req.body.name], 
+        if(err){
+            console.log('err');
+        };
+        conn.query("INSERT INTO todo VALUES (? , ? )",
+        [req.body.id , req.body.title], 
         (err, results , fields) => {
             res.setHeader('Content-Type', 'application/JSON');
             res.write(JSON.stringify(results));
             res.end();
-
         });
         
     });
 });
 
 //取得所有users
-app.get('/',async (req,res) =>{
-    const usersId = req.query.users_id;
-    const usersName = req.query.users_name;
-    pool.getConnection((err,conn) =>{
-        conn.query(
-            "INSERT INTO users VALUES (? , ?)",
-            [usersId , usersName],
-            (err,result,fields) =>{
-                res.write("Hello World!");
-                res.end();
-            }
-        );
-    });
-});
+// app.get('/',async (req,res) =>{
+//     const usersId = req.query.users_id;
+//     const usersName = req.query.users_name;
+//     pool.getConnection((err,conn) =>{
+//         conn.query(
+//             "INSERT INTO users VALUES (? , ?)",
+//             [usersId , usersName],
+//             (err,result,fields) =>{
+//                 res.write("Hello World!");
+//                 res.end();
+//             }
+//         );
+//     });
+// });
 
 //取得某個users
 app.get('/:id',async (req,res) =>{
